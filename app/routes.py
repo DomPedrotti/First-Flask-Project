62233497184e5f7d4ffba2aebc4020d53f0edac2
Dominic Pedrotti <pedrotti.dom@gmail.com>
2019-09-23 16:17:43 -0500

    used html to add greeting and blog posts

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username' : "Dom"}
+    posts = [
+        {
+            'author': {'username': 'Dom'},
+            'body' : 'Excited to start busting put this flask app'
+        },
+        {
+            'author' : {'username': 'Everyone Else'},
+            'body': 'Wow, Dom, this is so legit!' 
+
+        }
+    ]
+    return render_template('index.html', user = user, posts = posts)
