77ecbe28f78adff5d7a759fffe1752a1091e79d0
Dominic Pedrotti <pedrotti.dom@gmail.com>
2019-09-26 13:36:15 -0500

    added username and password areas

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,14 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('login requested for user {}, rmemeber_me {}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/')
 @app.route('/index')
@@ -20,7 +27,3 @@ def index():
     ]
     return render_template('index.html', user=user, posts = posts)
 
-@app.route('/login')
-def login():
-    form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
