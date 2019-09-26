9fc8c8b0c704dffef7b3c5f9ea8e85babc1704e7
Dominic Pedrotti <pedrotti.dom@gmail.com>
2019-09-26 10:38:26 -0500

    running into issue with login page

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import data_required
+from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators = [data_required()])
-    password = PasswordField('Password', validators = [data_required()])
+    username = StringField('Username', validators = [DataRequired()])
+    password = PasswordField('Password', validators = [DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('SignIn')
 
